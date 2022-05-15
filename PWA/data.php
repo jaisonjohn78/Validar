74c59b38a0b51cd6df723a9b7ae18a5467f5e812
jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-05-15 19:25:40 +0530

    QR Scanner Changes

---
+++ PWA/data.php
@@ -0,0 +1,12 @@
+<?php 
+    $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db="Validar";
+	$conn = mysqli_connect($servername, $username, $password,$db);
+
+    $qr_code = $_POST['qr_code'];
+     
+
+
+?>
