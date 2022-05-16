17f50774205656f2deaa1fadfc41d9e4c04190c4
Kareenabalwani <kareenabalwani5@gmail.com>
2022-05-16 18:02:07 +0530

    Cards and product_uploads

--- Manufacturer/index/cards-basic.php
+++ Manufacturer/index/cards-basic.php
@@ -1,3 +1,15 @@
+<?php
+
+include 'config.php';
+include 'function.php';
+$id = $_SESSION['man_data']->id;
+$sql ="SELECT * FROM `product` WHERE `uid`=$id";
+$query = mysqli_query($con,$sql);
+
+
+
+?>
+
 <!DOCTYPE html>
 
 <!-- beautify ignore:start -->
@@ -149,11 +161,23 @@
               <!-- Grid Card -->
               <h6 class="pb-1 mb-4 text-muted">All Products</h6>
               <div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
+
+              
+              <?php
+              
+              
+              while ($data = $query->fetch_object()) {
+                $users[] = $data;
+              }
+
+                foreach($users as $user){
+                ?>                      
+                 
                 <div class="col">
                   <div class="card h-100">
                     <img class="card-img-top" src="../assets/img/elements/2.jpg" alt="Card image cap" />
                     <div class="card-body">
-                      <h5 class="card-title">Card first title</h5>
+                      <h5 class="card-title"><?php echo $user->company_name;?></h5>
                       <p class="card-text">
                         This is a longer card with supporting text below as a natural lead-in to additional content.
                         This content is a little bit longer.
@@ -161,66 +185,15 @@
                     </div>
                   </div>
                 </div>
-                          <div class="col">
-                            <div class="card h-100">
-                              <img class="card-img-top" src="../assets/img/elements/13.jpg" alt="Card image cap" />
-                              <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                  This is a longer card with supporting text below as a natural lead-in to additional content.
-                                  This content is a little bit longer.
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                <div class="col">
-                  <div class="card h-100">
-                    <img class="card-img-top" src="../assets/img/elements/4.jpg" alt="Card image cap" />
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">
-                        This is a longer card with supporting text below as a natural lead-in to additional content.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="card h-100">
-                    <img class="card-img-top" src="../assets/img/elements/18.jpg" alt="Card image cap" />
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">
-                        This is a longer card with supporting text below as a natural lead-in to additional content.
-                        This content is a little bit longer.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <!-- <div class="col">
-                  <div class="card h-100">
-                    <img class="card-img-top" src="../assets/img/elements/19.jpg" alt="Card image cap" />
-                    <div class="card-body">
-                      <h5 class="card-title">Cardinggg title</h5>
-                      <p class="card-text">
-                        This is a longer card with supporting text below as a natural lead-in to additional content.
-                        This content is a little bit longer.
-                      </p>
-                    </div>
-                  </div>
-                </div> -->
-                <div class="col">
-                  <div class="card h-100">
-                    <img class="card-img-top" src="../assets/img/elements/20.jpg" alt="Card image cap" />
-                    <div class="card-body">
-                      <h5 class="card-title">Card last title</h5>
-                      <p class="card-text">
-                        This is a longer card with supporting text below as a natural lead-in to additional content.
-                        This content is a little bit longer.
-                      </p>
-                    </div>
-                  </div>
-                </div>
+              
+                <?php
+              
+                }
+              
+              ?>
               </div>
+              
+              
 
               <!-- Masonry -->
               <!-- <h6 class="pb-1 mb-4 text-muted">Masonry</h6> -->
